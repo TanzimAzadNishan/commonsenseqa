23b13d786dbb99a5ae3dce18cd46d2d23d4d6d08
TanzimAzadNishan <1705074@ugrad.cse.buet.ac.bd>
2022-12-29 21:05:13 +0600

    converting code to tensorflow 2.0

--- bert/modeling.py
+++ bert/modeling.py
@@ -363,8 +363,10 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  return tf.contrib.layers.layer_norm(
-      inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
+  norm_layer = tf_main.keras.layers.LayerNormalization(
+      axis=-1, name=name
+  )
+  return norm_layer(input_tensor)
 
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
